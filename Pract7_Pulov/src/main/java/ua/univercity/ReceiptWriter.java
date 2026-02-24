5d84745afdcd90f9f296099e904ac119b56d4022
mykhailopulov <mihail.pulov.247@gmail.com>
2026-02-24 10:53:50 +0200

    Pract7: Task Three corrected

--- Pract7_Pulov/src/main/java/ua/univercity/ReceiptWriter.java
+++ Pract7_Pulov/src/main/java/ua/univercity/ReceiptWriter.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 public class ReceiptWriter implements AutoCloseable {
 
     public void write(Order order) throws IOException {
-        System.out.println("[WRITER] Writing receipt for order #" + order.id());
+        System.out.println("Writing receipt for order #" + order.id());
     }
 
     @Override
     public void close() {
-        System.out.println("[CLOSE] ReceiptWriter is closed");
+        System.out.println("ReceiptWriter is closed");
     }
 }
